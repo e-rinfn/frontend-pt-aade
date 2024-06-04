5a321c9431eaa94662877b671cb56a64fd328841
Erin Fajrin Nugraha <erinfajrinnugraha@gmail.com>
2024-06-04 23:00:14 +0700

    menambahkan fungsi CRUD

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
     public function showLoginForm()
     {
         return view('auth.login');
@@ -20,19 +27,26 @@ class LoginController extends Controller
             'password' => 'required',
         ]);
 
-        // Kirim permintaan login ke API
-        $response = Http::post('http://localhost:8001/api/login', [
+         // Mengirim permintaan ke API untuk memverifikasi kredensial
+         $response = Http::post('http://localhost:8001/api/login', [
             'email' => $request->email,
             'password' => $request->password,
         ]);
 
-        // Memeriksa response dari API
-        if ($response->successful()){
-            // Login berhasil dilakukan
-            return redirect()->route('home');
+        // Memeriksa respons dari API
+        if ($response->successful()) {
+             // Login berhasil, ambil token
+             $token = $response->json()['token'];
+             // Hapus token sebelumnya (jika ada) dan simpan token baru dalam session
+             $request->session()->invalidate(); // Hapus session yang ada
+             $request->session()->regenerateToken(); // Regenerasi token session baru
+             $request->session()->put('api_token', $token); // Simpan token baru
+ 
+             // Alihkan ke halaman home
+             return redirect()->route('barangs.index');
         }
 
-        // Jika login gagal, kembali ke halaman sebelumnya dengan pesan error
+        // Login gagal, kembali ke halaman login dengan pesan error
         return back()->withErrors([
             'email' => 'The provided credentials do not match our records.',
         ]);
